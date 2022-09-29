ffe02c41919bd9348a18c2a51e5801734217b6ff
Alexandre Hilário <77108141+AlexandreHilario@users.noreply.github.com>
2022-09-29 19:31:31 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-library.add(fas)
-createApp(App)
-    .use(router)
-    .mount('#app')
+createApp(App).use(router).mount('#app')
     

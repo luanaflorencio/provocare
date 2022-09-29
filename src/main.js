33e1a0a4975b97730ce8ed8602c86f38ac0ae190
Alexandre Hilário <77108141+AlexandreHilario@users.noreply.github.com>
2022-09-29 18:57:38 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 import { createApp } from 'vue'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library} from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons' 
 import App from './App.vue'
 import router from './router'
 
 library.add(fas)
 createApp(App)
     .use(router)
-    .component('fa', FontAwesomeIcon)
     .mount('#app')
     

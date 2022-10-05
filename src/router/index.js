433ebebea3af6625f4212ab3b2a0761a5eea3780
Gabriel Henrique <gabrieltjs75@gmail.com>
2022-10-05 00:18:44 -0300

    Adicionado Pagina Login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Register from '../views/Register.vue'
+import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/register',
     name: 'register',
     component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 

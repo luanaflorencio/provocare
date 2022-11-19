1c4bbe362880aa51ebeb9db1b3f00c5b157f7e4f
luanaflorencio <luanaflorenciopc@gmail.com>
2022-11-18 22:40:53 -0300

    Iniciando tela dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,9 @@
 <template>
 <h1>Dashboard</h1>
+<div class="card-post">
+<input type="text" placeholder="Digite algo...">
+<input type="submit" class="btn" value="Postar">
+</div>
 </template>
 
 <script>

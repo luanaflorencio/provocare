151ddf4c66c2c2267fefa69ed9be29cc3b7edf1f
alexandre <alexandreds969@gmail.com>
2022-11-22 20:38:48 -0300

    icones adicionados em todos botôes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,10 +2,58 @@
   <main>
     <div>
       <div class="container">
-        <button class="btn-card">Home</button>
-        <button class="btn-card">Desafios</button>
-        <button class="btn-card">Respostas</button>
-        <button class="btn-card">Seus Desafios</button>
+        <button class="btn-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            style="fill: #fff; transform: ; msfilter: "
+          >
+            <path
+              d="M5 22h14a2 2 0 0 0 2-2v-9a1 1 0 0 0-.29-.71l-8-8a1 1 0 0 0-1.41 0l-8 8A1 1 0 0 0 3 11v9a2 2 0 0 0 2 2zm5-2v-5h4v5zm-5-8.59 7-7 7 7V20h-3v-5a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v5H5z"
+            ></path></svg
+          >Home
+        </button>
+        <button class="btn-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            style="fill: #fff; transform: ; msfilter: "
+          >
+            <path
+              d="m21.29 4.1-4.12-2a1.36 1.36 0 0 0-.48-.1h-.08a1.18 1.18 0 0 0-.72.24l-.14.12-7.88 7.19L4.44 7a.83.83 0 0 0-.54-.17.88.88 0 0 0-.53.17l-1.1 1a.8.8 0 0 0-.27.61.84.84 0 0 0 .27.62l3 2.71-3 2.72a.84.84 0 0 0 0 1.23l1.1 1a.89.89 0 0 0 .6.22.93.93 0 0 0 .47-.17l3.43-2.61 7.88 7.19a1.2 1.2 0 0 0 .76.36h.17a1 1 0 0 0 .49-.12l4.12-2a1.25 1.25 0 0 0 .71-1.1V5.23a1.26 1.26 0 0 0-.71-1.13zM17 16.47l-6-4.53 6-4.53z"
+            ></path></svg
+          >Desafios
+        </button>
+        <button class="btn-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="26"
+            height="26"
+            viewBox="0 0 26 26"
+            style="fill: #fff; transform: ; msfilter: "
+          >
+            <path
+              d="M3.25 3.25H20.0417V5.95833H18.9583V4.33333H4.33333V16.7917H15.918V17.875H3.25V3.25ZM19.5 10.2917C19.931 10.2917 20.3443 10.1205 20.649 9.81572C20.9538 9.51097 21.125 9.09764 21.125 8.66667C21.125 8.23569 20.9538 7.82236 20.649 7.51762C20.3443 7.21287 19.931 7.04167 19.5 7.04167C19.069 7.04167 18.6557 7.21287 18.351 7.51762C18.0462 7.82236 17.875 8.23569 17.875 8.66667C17.875 9.09764 18.0462 9.51097 18.351 9.81572C18.6557 10.1205 19.069 10.2917 19.5 10.2917ZM20.6001 11.3804C21.3037 11.3804 21.8606 11.6968 22.2251 12.1853C22.5658 12.643 22.7099 13.2091 22.7424 13.7318C22.7743 14.268 22.7012 14.8052 22.5274 15.3135C22.3649 15.7847 22.0903 16.2722 21.6667 16.6064V21.9375C21.6671 22.1416 21.5906 22.3384 21.4525 22.4888C21.3144 22.6391 21.1247 22.7319 20.9213 22.7488C20.7178 22.7657 20.5155 22.7053 20.3545 22.5798C20.1935 22.4542 20.0857 22.2727 20.0525 22.0713L19.3538 17.875H19.1923L18.4026 22.087C18.3651 22.2858 18.2548 22.4634 18.0932 22.5852C17.9317 22.707 17.7305 22.7641 17.5291 22.7454C17.3277 22.7267 17.1404 22.6335 17.0041 22.4841C16.8677 22.3347 16.7919 22.1398 16.7917 21.9375V14.2095C16.6833 14.3745 16.5765 14.5405 16.471 14.7073L16.4288 14.774L16.4179 14.7913L16.4152 14.7962C16.3422 14.9134 16.2405 15.0101 16.1198 15.0771C15.999 15.1441 15.8632 15.1792 15.7251 15.1791H13.0168C12.8013 15.1791 12.5946 15.0935 12.4423 14.9411C12.2899 14.7888 12.2043 14.5821 12.2043 14.3666C12.2043 14.1511 12.2899 13.9445 12.4423 13.7921C12.5946 13.6397 12.8013 13.5541 13.0168 13.5541H15.2793C15.411 13.3505 15.5843 13.0861 15.7663 12.8207C15.9559 12.5439 16.165 12.2509 16.3513 12.0212C16.4418 11.9091 16.543 11.7921 16.6449 11.6962C16.6947 11.6491 16.764 11.5879 16.8485 11.5337C16.9977 11.4358 17.1718 11.3828 17.3501 11.381H20.6001V11.3804Z"
+            /></svg
+          >Respostas
+        </button>
+        <button class="btn-card">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            style="fill: #fff; transform: ; msfilter: "
+          >
+            <path
+              d="M15 11h7v2h-7zm1 4h6v2h-6zm-2-8h8v2h-8zM4 19h10v-1c0-2.757-2.243-5-5-5H7c-2.757 0-5 2.243-5 5v1h2zm4-7c1.995 0 3.5-1.505 3.5-3.5S9.995 5 8 5 4.5 6.505 4.5 8.5 6.005 12 8 12z"
+            ></path></svg
+          >Seus Desafios
+        </button>
       </div>
     </div>
 
@@ -13,6 +61,32 @@
       <div class="wrapper">
         <div class="input-box">
           <div class="post-card">
+            <div class="icon-image">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                style="fill: #fff; transform: ; msfilter: "
+              >
+                <path
+                  d="M12.707 17.293 8.414 13H18v-2H8.414l4.293-4.293-1.414-1.414L4.586 12l6.707 6.707z"
+                ></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                style="fill: cornflowerblue; transform: ; msfilter: "
+              >
+                <circle cx="7.499" cy="9.5" r="1.5"></circle>
+                <path d="m10.499 14-1.5-2-3 4h12l-4.5-6z"></path>
+                <path
+                  d="M19.999 4h-16c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zm-16 14V6h16l.002 12H3.999z"
+                ></path>
+              </svg>
+            </div>
             <input type="text" class="input" placeholder="Digite algo..." />
             <input type="submit" class="btn" value="Postar" />
           </div>
@@ -57,9 +131,9 @@
           <img src="../assets/C_Hello_World_Program.png" alt="" />
         </div>
         <div class="buttons">
-            <button class="btnAction">Comentar</button>
-            <button class="btnAction">Solucionar</button>
-          </div>
+          <button class="btnAction">Comentar</button>
+          <button class="btnAction">Solucionar</button>
+        </div>
       </div>
     </div>
   </main>
@@ -96,7 +170,6 @@ export default {
   margin-top: 5rem;
   width: 60%;
   height: 100vh;
-  
 }
 
 .userPost {
@@ -126,9 +199,9 @@ export default {
   cursor: pointer;
 }
 
-svg {
-    width: 2rem;
-    height: auto;
+.user svg {
+  width: 2rem;
+  height: auto;
 }
 
 .infoTextPost {
@@ -148,8 +221,8 @@ svg {
 .buttons {
   display: flex;
   justify-content: space-around;
-  border-top: .0625rem solid #808080;
-  border-bottom: .0625rem solid #808080;
+  border-top: 0.0625rem solid #808080;
+  border-bottom: 0.0625rem solid #808080;
   margin-bottom: 1rem;
 }
 
@@ -164,9 +237,9 @@ svg {
 }
 
 .btnAction:hover {
-    transition: 0.2s;
-    background: rgba(31, 31, 31, 0.4);
-    transform: scale(1.05);
+  transition: 0.2s;
+  background: rgba(31, 31, 31, 0.4);
+  transform: scale(1.05);
 }
 
 h1 {
@@ -186,11 +259,12 @@ main {
   box-shadow: 6px 4px 5px rgba(0, 0, 0, 0.25);
   min-width: 50%;
   border-radius: 20px;
-  width: 40%;
+  /* width: 40%; */
   border-radius: 15px;
   padding: 25px 25px 15px 25px;
   margin-bottom: 2rem;
 }
+
 
 .input-box {
   padding-top: 20px;
@@ -200,6 +274,20 @@ main {
   position: relative;
   min-height: 120px;
   max-height: 150px;
+}
+
+.icon-image{
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 8rem;
+  cursor: pointer;
+}
+
+.post-card{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 .post-card .input {
@@ -238,9 +326,10 @@ main {
 .btn-card {
   background-color: #6464ac;
   width: 60%;
+  min-width: 100px;
   height: 5%;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   border: none;
   font-size: 1.2em;
   margin-left: 20%;
@@ -248,5 +337,6 @@ main {
   cursor: pointer;
   border-radius: 16px;
   box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
+  align-items: center;
 }
 </style>
